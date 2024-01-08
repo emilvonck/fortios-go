cfcccea6f6e757321aaff93c69daa1b7baac7eaa
Emil Vonck <emil.vonck@gmail.com>
2024-01-08 20:02:41 +0100

    renamed Ports struct

--- managed_switch.go
+++ managed_switch.go
@@ -28,7 +28,7 @@ type IgmpSnoopingGroup struct {
 type DhcpSnooping struct {
 	Untrusted int `json:"untrusted"`
 }
-type Ports struct {
+type Port struct {
 	Interface         string            `json:"interface"`
 	FortilinkPort     bool              `json:"fortilink_port"`
 	Vlan              string            `json:"vlan"`
@@ -51,25 +51,25 @@ type Ports struct {
 	DhcpSnooping      DhcpSnooping      `json:"dhcp_snooping"`
 }
 type Switch struct {
-	Status                string  `json:"status"`
-	OsVersion             string  `json:"os_version"`
-	ConnectingFrom        string  `json:"connecting_from,omitempty"`
-	JoinTime              string  `json:"join_time"`
-	ImageDownloadProgress int     `json:"image_download_progress"`
-	Name                  string  `json:"name"`
-	Serial                string  `json:"serial"`
-	FgtPeerIntfName       string  `json:"fgt_peer_intf_name"`
-	State                 string  `json:"state"`
-	Ports                 []Ports `json:"ports"`
-	MaxPoeBudget          int     `json:"max_poe_budget"`
-	IgmpSnoopingSupported bool    `json:"igmp_snooping_supported"`
-	DhcpSnoopingSupported bool    `json:"dhcp_snooping_supported"`
-	McLagSupported        bool    `json:"mc_lag_supported"`
-	LedBlinkSupported     bool    `json:"led_blink_supported"`
-	Type                  string  `json:"type"`
-	FaceplateXML          string  `json:"faceplate_xml"`
-	Vdom                  string  `json:"vdom"`
-	IsL3                  bool    `json:"is_l3"`
+	Status                string `json:"status"`
+	OsVersion             string `json:"os_version"`
+	ConnectingFrom        string `json:"connecting_from,omitempty"`
+	JoinTime              string `json:"join_time"`
+	ImageDownloadProgress int    `json:"image_download_progress"`
+	Name                  string `json:"name"`
+	Serial                string `json:"serial"`
+	FgtPeerIntfName       string `json:"fgt_peer_intf_name"`
+	State                 string `json:"state"`
+	Ports                 []Port `json:"ports"`
+	MaxPoeBudget          int    `json:"max_poe_budget"`
+	IgmpSnoopingSupported bool   `json:"igmp_snooping_supported"`
+	DhcpSnoopingSupported bool   `json:"dhcp_snooping_supported"`
+	McLagSupported        bool   `json:"mc_lag_supported"`
+	LedBlinkSupported     bool   `json:"led_blink_supported"`
+	Type                  string `json:"type"`
+	FaceplateXML          string `json:"faceplate_xml"`
+	Vdom                  string `json:"vdom"`
+	IsL3                  bool   `json:"is_l3"`
 }
 
 func (c *Client) GetManagedSwitch(ctx context.Context) (*ManagedSwitchResponse, error) {
